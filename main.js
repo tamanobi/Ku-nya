2909d352c4ca8eee0afac90d96208a42508eda69
Kohki YAMAGIWA <tamanobi@gmail.com>
2017-06-06 03:57:08 +0900

    add filtering Manga tag

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 'use restrict';
 
-function init() {
-  const SOURCE_URI = 'https://www.pixiv.net/ranking.php?mode=daily&format=json&content=illust';
+function includes(sequence, needle) {
+    //console.log(sequence.length);
+  return sequence.some(e => {
+      console.log(e, e.includes(needle), needle);
+    return e === needle;
+  });
+}
+
+function init(content, _with_manga_tag, _with_big_height) {
+  let with_manga_tag = (typeof _with_manga_tag !== undefined)?_with_manga_tag:false;
+  let with_big_height = (typeof _with_big_height !== undefined)?_with_big_height:false;
+    console.log(with_manga_tag);
+  let SOURCE_URI = `https://www.pixiv.net/ranking.php?mode=daily&format=json&content=${content}`;
   const promiseList = [];
 
   for (let i = 1; i <= 3; i++) {
+    // cannnot fetch ugoira ranking over page 3.
+    if (content === "ugoira" && i > 2) {
+        break;
+    }
     const p = axios.get(`${SOURCE_URI}&p=${i}`);
     promiseList.push(p);
   }
@@ -14,16 +29,32 @@ function init() {
     let illustrations = [];
 
     values = values.filter(response => {
-        return response.status === 200;
+      return response.status === 200;
     });
 
     values.forEach(response => {
+      let filtered = response.data.contents;
+      if (!with_manga_tag) {
+        filtered = filtered.filter(content => {
+          // OK -> return (includes(content.tags, 'Fate/GrandOrder') === true);
+          // NG -> return (includes(content.tags, '漫画') === true);
+          return (content.tags.includes('漫画') === false);
+        });
+      }
+
+      if (!with_big_height) {
+        filtered = filtered.filter(content => {
+          return (content.height / content.width <= 3);
+        });
+      }
+
       // Extracting illustration info
-      response.data.contents.forEach(content => {
+      filtered.forEach(content => {
           illustrations.push({
             'illustrationId': content.illust_id,
             'illustrationUrl': content.url,
             'illustrationTitle': content.title,
+            'illustrationTags': content.tags,
             'userName': content.user_name,
           });
       });
@@ -38,6 +69,7 @@ function init() {
         illust.illustrationId,
         illust.illustrationUrl,
         illust.illustrationTitle,
+        illust.illustrationTags,
         illust.userName
       ));
     });
@@ -62,8 +94,53 @@ function init() {
   });
 }
 
+function resetGallery() {
+    const gallery = document.querySelector('#gallery');
+    while (gallery.firstChild) {
+        gallery.removeChild(gallery.firstChild);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', event => {
-  init();
+  var v = new Vue({
+      el: '#setting',
+      data: {
+          selected: (window.localStorage && window.localStorage.getItem('content')) ? window.localStorage.getItem('content') : 'illust',
+          options: [
+              {value: 'illust'},
+              {value: 'ugoira'},
+              {value: 'manga'},
+          ],
+          with_manga_tag: (window.localStorage && window.localStorage.getItem('with_manga_tag')) ? window.localStorage.getItem('with_manga_tag') : false,
+          with_big_height: (window.localStorage && window.localStorage.getItem('with_big_height')) ? window.localStorage.getItem('with_big_height') : false,
+      },
+      watch: {
+          with_manga_tag: function (newChecked){
+               if (window.localStorage) {
+                   window.localStorage.setItem('with_manga_tag', newChecked);
+               }
+               resetGallery();
+               console.log(`manga: ${this.with_manga_tag}`);
+               init(this.selected, this.with_manga_tag, this.with_big_height);
+          },
+          with_big_height: function (newChecked){
+               if (window.localStorage) {
+                   window.localStorage.setItem('with_big_height', newChecked);
+               }
+               resetGallery();
+               init(this.selected, this.with_manga_tag, this.with_big_height);
+          },
+          selected: function (newSelected){
+               if (window.localStorage) {
+                   window.localStorage.setItem('content', newSelected);
+               }
+               resetGallery();
+               init(newSelected, this.with_manga_tag, this.with_big_height);
+          }
+      }
+  });
+
+  init(v.selected, v.with_manga_tag, v.with_big_height);
 });
 
 function createIllustrationElement(imageUrl, title, author) {
@@ -73,9 +150,10 @@ function createIllustrationElement(imageUrl, title, author) {
   return img;
 }
 
-function createGalleryItem(illustrationId, imageUrl, title, author) {
+function createGalleryItem(illustrationId, imageUrl, title, tags, author) {
     const linkUrl = `https://www.pixiv.net/i/${illustrationId}`;
     const img = createIllustrationElement(imageUrl, title, author);
+    img.setAttribute('alt', tags.join(' '));
     const anchor = document.createElement('a');
     anchor.setAttribute('href', linkUrl);
     anchor.setAttribute('target', 'pixiv');
