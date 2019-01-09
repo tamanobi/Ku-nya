c18c58f5b0a78049c54331a3b5f18f8b1787965d
tamanobi <tamanobi@gmail.com>
2019-01-09 09:19:41 +0900

    can show new illustrations when logout

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import * as cheerio from 'cheerio'
 
 export interface IllustEntry {
   id: number
@@ -23,33 +22,27 @@ export const getNewIllusts = async (): Promise<IllustEntry[]> => {
     axios.get(`${URL}&p=4`),
   ])
 
-  const illusts = responses
-    .map(({ data }) => {
-      const $ = cheerio.load(data)
-      const items = $('._image-items > li')
-
-      return Array.from(items).map(el => {
-        const $img = cheerio(el).find('img._thumbnail')
-
-        return {
-          id: +$img.data('id'),
-          imageUrl: $img.data('src').replace(/\/c\/.+?\//, '/c/480x960/'),
-          title: cheerio(el)
-            .find('h1')
-            .text(),
-          tags: $img.data('tags') ? $img.data('tags').split(' ') : [],
-          authorName: cheerio(el)
-            .find('h1')
-            .text(),
-          // TODO: This is stub. Can't get image size from new_illust.php response.
-          width: 1,
-          height: 1,
-        } as IllustEntry
-      })
-    })
+  return responses
+    .filter(res => res.status == 200)
+    .map(res =>
+      res.data.filter(content => typeof content.illust_id !== 'undefined'),
+    )
+    .map(res =>
+      res.map(
+        (content): IllustEntry => ({
+          id: content.illust_id,
+          imageUrl: content.url
+            .replace(/c\/\d+x\d+_\d+_\w+\//, `c/${imageResolution}/`)
+            .replace('_square', '_master'),
+          title: content.title,
+          tags: content.tags,
+          width: content.illust_width,
+          height: content.illust_height,
+          authorName: content.user_name,
+        }),
+      ),
+    )
     .reduce((l, r) => l.concat(...r), []) // flatten
-
-  return illusts
 }
 
 export const getPopularIllusts = async (): Promise<IllustEntry[]> => {
