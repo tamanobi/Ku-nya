f5e236ce58f56ccf8cab5ffc656f754e0e16e328
tamanobi <tamanobi@gmail.com>
2019-01-28 09:14:57 +0900

    use service worker

--- src/sw.js
+++ src/sw.js
@@ -1,19 +1,14 @@
-console.log('startup')
-
 CACHE_NAME = 'Ku-nya-cache-v1'
 
 self.addEventListener('install', event => {
-  console.log('install')
   event.waitUntil(
     (async function() {
       const cache = await caches.open(CACHE_NAME)
-      console.log('open cache')
     })(),
   )
 })
 
 self.addEventListener('activate', event => {
-  console.log('activate')
   event.waitUntil(
     (async function() {
       const cacheNames = await caches.keys()
@@ -21,9 +16,6 @@ self.addEventListener('activate', event => {
         cacheNames
           .filter(cacheName => {
             return false
-            // Return true if you want to remove this cache,
-            // but remember that caches are shared across
-            // the whole origin
           })
           .map(cacheName => {
             caches.delete(cacheName)
@@ -42,13 +34,12 @@ self.addEventListener('fetch', event => {
     return event.respondWith(
       caches.open(CACHE_NAME).then(cache => {
         return cache.match(event.request).then(response => {
-          // if (response) console.log('hit cache')
           return (
             response ||
             fetch(event.request).then(response => {
-              //cache.put(event.request, response.clone());
-              cache.add(event.request)
-              console.log('put cache')
+              if (response.status === 0 || response.status === 200) {
+                cache.put(event.request, response.clone())
+              }
               return response
             })
           )
