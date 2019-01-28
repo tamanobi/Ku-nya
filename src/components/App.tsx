e8f597dcc49c8d01951b2ef5cc80a2bf3c26c208
tamanobi <tamanobi@gmail.com>
2019-01-28 09:21:17 +0900

    remove unused

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -41,11 +41,9 @@ export default class App extends Component<Props, State> {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('sw.js')
       .then(registration => {
-        // 登録成功
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }).catch(err => {
-        // 登録失敗 :(
-        console.log('ServiceWorker registration failed: ', err);
+        // console.log('ServiceWorker registration failed: ', err);
       });
     }
 
--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,6 @@
 CACHE_NAME = 'Ku-nya-cache-v1'
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    (async function() {
-      const cache = await caches.open(CACHE_NAME)
-    })(),
-  )
-})
+self.addEventListener('install', () => {})
 
 self.addEventListener('activate', event => {
   event.waitUntil(
@@ -15,7 +9,7 @@ self.addEventListener('activate', event => {
       await Promise.all(
         cacheNames
           .filter(cacheName => {
-            return false
+            return true
           })
           .map(cacheName => {
             caches.delete(cacheName)
@@ -25,9 +19,6 @@ self.addEventListener('activate', event => {
   )
 })
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', event => {
   const requestURL = new URL(event.request.url)
   if (requestURL.hostname === 'i.pximg.net') {
